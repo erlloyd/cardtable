b05c0ff7a501ba416b79431f4160e4a42c624fc3
Eric Lloyd <erlloyd@gmail.com>
2018-08-20 22:57:09 -0500

    just starting

---
+++ src/reducers/initialState.js
@@ -0,0 +1,22 @@
+export default {
+  cards: [
+    {
+      id: 0,
+      x: 200,
+      y: 200,
+      fill: 'red',
+      exhausted: false,
+      selected: false,
+      dragging: false
+    },
+    {
+      id: 1,
+      x: 400,
+      y: 400,
+      fill: 'red',
+      exhausted: false,
+      selected: false,
+      dragging: false
+    }
+  ]
+}
