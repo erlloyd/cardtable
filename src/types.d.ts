2afb3a2da351cad79cff17561a1082f6b87a73c2
Eric Lloyd <erlloyd@gmail.com>
2018-11-22 17:16:56 -0600

    Updates

--- src/types.d.ts
+++
@@ -1,8 +0,0 @@
-import { StateType } from 'typesafe-actions';
-import { CardsAction } from './features/cards';
-import rootReducer from './reducers/rootReducer';
-
-declare module 'Types' {
-  export type RootState = StateType<typeof rootReducer>;
-  export type RootAction = CardsAction;
-}
