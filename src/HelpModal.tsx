d78363397146e358434df83900bc113f88f5f8ba
Eric Lloyd <erlloyd@gmail.com>
2021-04-19 16:23:45 -0500

    Placeholder for Help Modal

---
+++ src/HelpModal.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+interface IProps {
+  open: boolean;
+}
+
+const HelpModal = (props: IProps) => {
+  return props.open ? <div>MODAL</div> : null;
+};
+
+export default HelpModal;
