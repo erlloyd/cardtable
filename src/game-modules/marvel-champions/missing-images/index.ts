90bab70a005fac0aa1b39e0a756712877eaa6630
Eric Lloyd <erlloyd@gmail.com>
2023-05-02 18:05:21 -0500

    Add missing tower defense images

--- src/game-modules/marvel-champions/missing-images/index.ts
+++ src/game-modules/marvel-champions/missing-images/index.ts
@@ -1,11 +1,13 @@
 import ScarletWitch from "./scarlet-witch";
 import GalaxiesMostWanted from "./galaxies-most-wanted";
 import KitchenSink from "./kitchensink";
+import MadTitansShadow from "./mad-titans-shadow";
 
 const MISSING_CARD_IMAGE_MAP = {
   ...ScarletWitch,
-  ...KitchenSink,
   ...GalaxiesMostWanted,
+  ...MadTitansShadow,
+  ...KitchenSink,
 };
 
 export default MISSING_CARD_IMAGE_MAP;
