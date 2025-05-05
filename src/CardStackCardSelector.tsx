ca7209f2abf13e74f51d20efa80aca738afe8905
Eric Lloyd <erlloyd@gmail.com>
2025-05-05 15:05:56 -0500

    Revert "Attempt to bring back changelog, and do some internal refactoring."
    This reverts commit 58877aa9aaa96f2b5da0d80b5f6be2c47f51772d.

--- src/CardStackCardSelector.tsx
+++ src/CardStackCardSelector.tsx
@@ -2,7 +2,8 @@ import { Button } from "@mui/material";
 import TextField from "@mui/material/TextField";
 import cx from "classnames";
 import Fuse from "fuse.js";
-import React, { useState, useEffect, useCallback } from "react";
+import * as React from "react";
+import { Component } from "react";
 import "./CardStackCardSelector.scss";
 import TopLayer from "./TopLayer";
 import { ICardData } from "./features/cards-data/initialState";
@@ -26,129 +27,144 @@ interface IProps {
   touchBased: boolean;
 }
 
-const CardStackCardSelector: React.FC<IProps> = (props) => {
-  const [currentSearchString, setCurrentSearchString] = useState("");
+interface IState {
+  currentSearchString: string;
+}
 
-  useEffect(() => {
-    return () => {
-      props.clearPreview();
+class CardStackCardSelector extends Component<IProps, IState> {
+  static whyDidYouRender = false;
+
+  constructor(props: IProps) {
+    super(props);
+
+    this.state = {
+      currentSearchString: "",
     };
-  }, [props]);
-
-  const handleReset = useCallback((e: React.MouseEvent) => {
-    e.stopPropagation();
-    setCurrentSearchString("");
-  }, []);
-
-  const handleSearchTermChange = useCallback(
-    (event: React.ChangeEvent<HTMLInputElement>) => {
-      setCurrentSearchString(event.target.value);
-    },
-    []
-  );
-
-  const cancelBubble = useCallback((event: React.SyntheticEvent) => {
-    event.stopPropagation();
-  }, []);
-
-  const haystack = props.card.cardStack.map((details) => ({
-    jsonId: details.jsonId,
-    name: props.cardsDataEntities[details.jsonId].name,
-  }));
-
-  let filteredResults = haystack;
-
-  if (currentSearchString !== "") {
-    const options = {
-      keys: ["name"], // Search in `name`
-    };
-
-    const f = new Fuse(haystack, options);
-    filteredResults = f.search(currentSearchString).map((r) => r.item);
   }
 
-  return (
-    <TopLayer
-      staticPosition={true}
-      trasparentBackground={true}
-      blurBackground={true}
-      offsetContent={false}
-      position={{ x: 0, y: 0 }}
-      noPadding={true}
-      fullHeight={true}
-      fullWidth={true}
-      completed={props.completed}
-    >
-      <div className="card-selector-wrapper">
-        <div className="text-field-wrapper">
-          <div className="text-field-background">
-            <TextField
-              value={currentSearchString}
-              variant="outlined"
-              onClick={cancelBubble}
-              onKeyDown={cancelBubble}
-              onKeyUp={cancelBubble}
-              onChange={handleSearchTermChange}
-            ></TextField>
-          </div>
-          <Button variant="contained" onClick={handleReset}>
-            Reset
-          </Button>
-        </div>
+  componentWillUnmount() {
+    this.props.clearPreview();
+  }
 
-        <div className="card-images-selector">
-          {filteredResults.map((cardDetails, index) => {
-            const imgUrl =
-              getImgUrlsFromJsonId(
+  render() {
+    const haystack = this.props.card.cardStack.map((details) => ({
+      jsonId: details.jsonId,
+      name: this.props.cardsDataEntities[details.jsonId].name,
+    }));
+
+    let filteredResults = haystack;
+
+    if (this.state.currentSearchString !== "") {
+      const options = {
+        // Search in `name`
+        keys: ["name"],
+      };
+
+      const f = new Fuse(haystack, options);
+      filteredResults = f
+        .search(this.state.currentSearchString)
+        .map((r) => r.item);
+    }
+    return (
+      <TopLayer
+        staticPosition={true}
+        trasparentBackground={true}
+        blurBackground={true}
+        offsetContent={false}
+        position={{ x: 0, y: 0 }}
+        noPadding={true}
+        fullHeight={true}
+        fullWidth={true}
+        completed={this.props.completed}
+      >
+        <div className="card-selector-wrapper">
+          <div className="text-field-wrapper">
+            <div className="text-field-background">
+              <TextField
+                value={this.state.currentSearchString}
+                variant="outlined"
+                onClick={this.cancelBubble}
+                onKeyDown={this.cancelBubble}
+                onKeyUp={this.cancelBubble}
+                onChange={this.handleSearchTermChange}
+              ></TextField>
+            </div>
+            <Button variant="contained" onClick={this.handleReset}>
+              Reset
+            </Button>
+          </div>
+
+          <div className="card-images-selector">
+            {filteredResults.map((cardDetails, index) => {
+              const imgUrl =
+                getImgUrlsFromJsonId(
+                  cardDetails.jsonId,
+                  true,
+                  this.props.cardsDataEntities,
+                  this.props.currentGameType
+                )[0] ?? "missing";
+
+              const cardType = getCardTypeWithoutStack(
                 cardDetails.jsonId,
                 true,
-                props.cardsDataEntities,
-                props.currentGameType
-              )[0] ?? "missing";
+                this.props.cardsDataEntities
+              );
 
-            const cardType = getCardTypeWithoutStack(
-              cardDetails.jsonId,
-              true,
-              props.cardsDataEntities
-            );
+              let shouldRotate = shouldRenderImageHorizontal(
+                cardDetails.jsonId,
+                cardType,
+                GameManager.horizontalCardTypes[
+                  this.props.currentGameType ??
+                    GameManager.allRegisteredGameTypes[0]
+                ],
+                imgUrl.includes("back")
+              );
 
-            let shouldRotate = shouldRenderImageHorizontal(
-              cardDetails.jsonId,
-              cardType,
-              GameManager.horizontalCardTypes[
-                props.currentGameType ?? GameManager.allRegisteredGameTypes[0]
-              ],
-              imgUrl.includes("back")
-            );
+              if (
+                !!this.props.currentGameType &&
+                GameManager.getModuleForType(this.props.currentGameType)
+                  .shouldRotateCard
+              ) {
+                shouldRotate = GameManager.getModuleForType(
+                  this.props.currentGameType
+                ).shouldRotateCard!(cardDetails.jsonId, cardType, true);
+              }
 
-            if (
-              !!props.currentGameType &&
-              GameManager.getModuleForType(props.currentGameType)
-                .shouldRotateCard
-            ) {
-              shouldRotate = GameManager.getModuleForType(props.currentGameType)
-                .shouldRotateCard!(cardDetails.jsonId, cardType, true);
-            }
-
-            return (
-              <img
-                key={`full-hand-image-${cardDetails.jsonId}-${index}`}
-                className={cx({
-                  "full-hand-img": true,
-                  "rotate-card": shouldRotate,
-                })}
-                src={imgUrl}
-                onClick={() => {
-                  props.cardSelected(cardDetails.jsonId);
-                }}
-                onDoubleClick={cancelBubble}
-              ></img>
-            );
-          })}
+              return (
+                <img
+                  key={`full-hand-image-${cardDetails.jsonId}-${index}`}
+                  className={cx({
+                    "full-hand-img": true,
+                    "rotate-card": shouldRotate,
+                  })}
+                  src={imgUrl}
+                  onClick={() => {
+                    this.props.cardSelected(cardDetails.jsonId);
+                  }}
+                  onDoubleClick={this.cancelBubble}
+                ></img>
+              );
+            })}
+          </div>
         </div>
-      </div>
-    </TopLayer>
-  );
-};
+      </TopLayer>
+    );
+  }
+
+  private handleReset = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    this.setState({ currentSearchString: "" });
+  };
+
+  private handleSearchTermChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    this.setState({ currentSearchString: event.target.value });
+  };
+
+  private cancelBubble = (event: React.SyntheticEvent) => {
+    event.stopPropagation();
+  };
+}
 
 export default CardStackCardSelector;
